aed853e770d8c0be3e6a1c305e7522d597a7ed57
Salmaan Abdi <salmaanabdihassan204@gmail.com>
2022-06-30 09:31:56 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+/**
+ *
+ *  * main - prints a string 
+ *
+ *   *description : prints "Programming is like building a multilingual puzzle" by puts function 
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
