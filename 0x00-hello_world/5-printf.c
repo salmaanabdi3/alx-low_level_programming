992e64611d5279f5f446a66064695a40445f2039
Salmaan Abdi <salmaanabdihassan204@gmail.com>
2022-06-30 10:06:33 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+* main - prints a string
+* description :print with proper grammar, but the outcome is a piece of art
+* by printf function
+* Return: 0
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art");
 	return (0);
 }
