d861ec83d2938fab32a08a6264a14ba90a9989e2
Salmaan Abdi <salmaanabdihassan204@gmail.com>
2022-07-03 08:35:12 -0700

    task102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints the combaination of three digits
+ * description :prints "prints the combanition of the three numbers"
+ * Return: 0
+ */
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 98; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 99; digit2++)
+		{
+			putchar(digit1 + '0');
+			putchar((digit2 / 10) + '0');
+			putchar(' ');
+			putchar(digit2 + '0');
+			putchar((digit2 / 10) + '0');
+			if (digit1 == 98 && digit2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
