7355f109192fe8312d4d84ecc5fdcb8a4d094c5e
Salmaan Abdi <salmaanabdihassan204@gmail.com>
2022-07-03 07:09:21 -0700

    fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,13 @@ int main(void)
 	int num;
 
 	for (num = 0; num < 10; num++)
+	{
 		putchar(num + '0');
 		if (num == 9)
 			continue;
 		putchar(',');
 		putchar(' ');
+	}
 	putchar('\n');
 	return (0);
 }
