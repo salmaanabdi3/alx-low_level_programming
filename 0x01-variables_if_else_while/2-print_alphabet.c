fe56fc4f7c381e647967ca9eb823d3909341d9cc
Salmaan Abdi <salmaanabdihassan204@gmail.com>
2022-07-03 05:44:44 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints the alphabet in lowercase,
+ * description :prints "prints the alphabet in lowercase,"
+ * Return: 0
+ */
+int main(void)
+{
+	char xaraf;
+
+	for (xaraf = 'a'; xaraf <= 'z'; xaraf++)
+		putchar(xaraf);
+	printf("\n");
+	return (0);
+}
